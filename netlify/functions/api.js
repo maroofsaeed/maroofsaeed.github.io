f05c741935c3c322e3e45f182ac4ca878bf32320
Maroof Saeed <maroof.saeed@gmail.com>
2023-10-18 16:27:10 +0500

    api change for api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -30,4 +30,26 @@ exports.handler = async (event, context) => {
         };
       }
     }
+
+    if(event.httpMethod === 'POST'){
+      try {
+        // Parse the incoming JSON payload from the request body
+        const requestBody = JSON.parse(event.body);
+  
+        // Save the data to a database or perform other necessary operations
+        // ...
+  
+        // Return a success response
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'POST request processed successfully' }),
+        };
+      } catch (error) {
+        // Return an error response if there was an issue processing the request
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Failed to process POST request' }),
+        };
+      }
+    }
   };
